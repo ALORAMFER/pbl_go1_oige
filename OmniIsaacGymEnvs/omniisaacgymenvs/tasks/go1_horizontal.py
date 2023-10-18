9b0bb777f4d79799ea34e0369aa6fd343c3e5abf
Alonso Ramos <aloramfer93@gmail.com>
2023-10-18 15:58:34 +0900

    jus add a comment line to test

--- OmniIsaacGymEnvs/omniisaacgymenvs/tasks/go1_horizontal.py
+++ OmniIsaacGymEnvs/omniisaacgymenvs/tasks/go1_horizontal.py
@@ -25,6 +25,7 @@
 # CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#This line is new
 
 from omniisaacgymenvs.tasks.base.rl_task import RLTask
 from omniisaacgymenvs.robots.articulations.go1 import Go1
